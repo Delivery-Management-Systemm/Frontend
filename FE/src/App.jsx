4fc2bc62023298c91738065c07db73b2c2b66155
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-12-03 19:06:52 +0700

    them account, chinh mau dáhboard

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login.jsx";
 import DashboardPage from "./pages/Dashboard.jsx";
+import Account from "./pages/Account.jsx";
 import "./App.css";
 
 function App() {
@@ -9,8 +11,21 @@ function App() {
   if (!isLoggedIn) {
     return <LoginPage onLogin={() => setIsLoggedIn(true)} />;
   }
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Dashboard là layout chính */}
+        <Route path="/" element={<DashboardPage />}>
+          {/* Trang mặc định bên trong Dashboard */}
+          <Route index element={<div>Chọn mục ở menu</div>} />
 
-  return <DashboardPage />;
+          {/* Trang Account */}
+          <Route path="account" element={<Account />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
